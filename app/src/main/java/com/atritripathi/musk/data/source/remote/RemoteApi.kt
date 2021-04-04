c655d02818d9940cf077cb31dd6a74e3ed95af80
AtriTripathi <tripathi.atri@gmail.com>
2021-04-04 17:43:10 +0530

    Setup `AppModule` using Dagger-Hilt.

--- app/src/main/java/com/atritripathi/musk/data/source/remote/RemoteApi.kt
+++ app/src/main/java/com/atritripathi/musk/data/source/remote/RemoteApi.kt
@@ -3,7 +3,7 @@ package com.atritripathi.musk.data.source.remote
 import com.atritripathi.musk.data.model.Rocket
 import retrofit2.http.GET
 
-interface SpaceXApi {
+interface RemoteApi {
 
     companion object {
         const val BASE_URL = "https://api.spacexdata.com/v4/"
---
+++ app/src/main/java/com/atritripathi/musk/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.atritripathi.musk.di
+
+import com.atritripathi.musk.data.source.remote.RemoteApi
+import com.atritripathi.musk.data.source.remote.RemoteApi.Companion.BASE_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteApi(retrofit: Retrofit): RemoteApi =
+        retrofit.create(RemoteApi::class.java)
+
+
+}
